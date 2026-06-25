dd6e32f98d7bc7bf8eb4d511459d3717985f00f5
andersonstack <andersong.pereiracruz@gmail.com>
2026-06-25 19:53:31 -0300

    feat: criando página de tela de matérias Closes #7

--- src/pages/Classroom.tsx
+++ src/pages/Classroom.tsx
@@ -1,5 +1,18 @@
+import { Title } from "@shared/components/Title";
+
+import CreateClassroom from "@shared/design-system/CreateClassroom";
+import ListClassroom from "@shared/design-system/ListClassroom";
+
 export default function ClassroomPage() {
   return (
-    <h1 className="text-black">Aqui será a página de cadastrar matérias</h1>
+    <section className="">
+      <Title
+        title="Matérias"
+        subTitle="Acompanhe suas matérias"
+        typeTitle="h1"
+      />
+      <CreateClassroom />
+      <ListClassroom />
+    </section>
   );
 }
