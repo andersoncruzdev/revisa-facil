c383407b2d7d9aaebd69e3c18497866f4aee8941
andersonstack <andersong.pereiracruz@gmail.com>
2026-07-18 19:48:34 -0300

    styles: atualizando estilo do card #12

--- src/features/classroom/ListClassroom.tsx
+++ src/features/classroom/ListClassroom.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useClassroom } from "@hooks/useClassroom";
-import { Badge } from "@shared/components/Badge";
 import { Button } from "@shared/components/Button";
 
 import { Trash } from "lucide-react";
@@ -17,24 +16,23 @@ export default function ListClassroom() {
   if (getClassroom.data?.length) {
     return (
       <section aria-label="lista de matérias" className="mt-6">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-4">
           {getClassroom.data.map((classroom) => (
             <div
               aria-label={`Divisão da matéria: ${classroom.name}`}
               key={classroom.id}
-              className="flex justify-between shadow-md p-10 gap-4"
+              className="flex items-center justify-between gap-4 rounded-xl border border-stone-300 bg-white py-6 pl-8 pr-6 sm:py-7 sm:pl-9 sm:pr-7"
+              style={{ boxShadow: `inset 6px 0 0 ${classroom.color}` }}
             >
               <div className="flex gap-2 items-center">
-                <Badge
-                  color={classroom.color}
-                  ariaLabel={`Cor da matéria ${classroom.name}`}
-                />
-                <p className="font-bold text-black uppercase">
-                  {classroom.name}
+                <p className="  font-semibold leading-tight text-blue-950">
+                  {classroom.name.toUpperCase()}
                 </p>
               </div>
               <Button.Root
                 aria-label={`Excluir matéria ${classroom.name}`}
+                color="red"
+                rounded
                 onClick={() => deleteWithId(classroom.id)}
               >
                 <Button.Icon icon={Trash} />
